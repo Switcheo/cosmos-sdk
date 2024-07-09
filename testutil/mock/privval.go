eb6f6ac1c71ac191d9d557c4c078abdd92885dcc
yan-soon <yan@Yans-MacBook-Pro.local>
2024-07-09 14:40:18 +0800

    update cometbft psuedo ver

--- testutil/mock/privval.go
+++ testutil/mock/privval.go
@@ -51,8 +51,8 @@ func (pv PV) SignProposal(chainID string, proposal *cmtproto.Proposal) error {
 }
 
 // SignOracleVote implements PrivValidator interface
-func (pv PV) SignOracleVote(_ string, oracleVote *oracleproto.GossipedVotes) error {
-	signBytes := cmttypes.OracleVoteSignBytes(oracleVote)
+func (pv PV) SignOracleVote(chainID string, oracleVote *oracleproto.GossipedVotes) error {
+	signBytes := cmttypes.OracleVoteSignBytes(chainID, oracleVote)
 	sig, err := pv.PrivKey.Sign(signBytes)
 	if err != nil {
 		return err
